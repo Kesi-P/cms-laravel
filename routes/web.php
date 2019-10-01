5f4e88e759b03d0d7e446f56f2c2ffdc584444df
Kesi Promnual <kesi.promnual@myzone.org>
2019-10-01 15:30:07 +0100

    Protected all routes br auth

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home'); //creat the homepage
-//register all the route from category and see the route by route:list
-Route::resource('categories','CategoriesController'); //index page will be home/categories
-Route::resource('posts','PostsController')->middleware('auth');
-Route::get('trashed-post','PostsController@trashed');  //register new route cuz resource doesn't provide ,on index file
-Route::put('restore-post/{post}','PostsController@restore'); //put use for security path, people can't direct it from url(like get or post)
+Route::group(['middleware' => ['auth']], function () {
+  Route::get('/home', 'HomeController@index')->name('home'); //creat the homepage
+  //register all the route from category and see the route by route:list
+  Route::resource('categories','CategoriesController'); //index page will be home/categories
+  Route::resource('posts','PostsController');
+  Route::get('trashed-post','PostsController@trashed');  //register new route cuz resource doesn't provide ,on index file
+  Route::put('restore-post/{post}','PostsController@restore'); //put use for security path, people can't direct it from url(like get or post)
+
+});
